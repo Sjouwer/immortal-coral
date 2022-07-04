a7bda8dbf92a47653cb8ba7518d376bbad4853df
Sjouwer <sjouwer1@gmail.com>
2022-07-04 16:30:22 +0200

    Only rename dead coral if it's coming from the smoker

--- src/main/java/io/github/sjouwer/immortalcoral/ImmortalCoral.java
+++ src/main/java/io/github/sjouwer/immortalcoral/ImmortalCoral.java
@@ -1,15 +1,47 @@
 package io.github.sjouwer.immortalcoral;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Style;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.GameRules;
+
+import java.util.HashMap;
 
 import static io.github.sjouwer.immortalcoral.mixin.GameRulesAccessor.invokeRegister;
 import static io.github.sjouwer.immortalcoral.mixin.BooleanRuleAccessor.invokeCreate;
 
 public class ImmortalCoral implements ModInitializer {
     public static final GameRules.Key<GameRules.BooleanRule> isCoralImmortal = invokeRegister("immortalCoral", GameRules.Category.MISC, invokeCreate(false));
+    private static final HashMap<String, String> smokedNamesMap = new HashMap<>();
 
     @Override
     public void onInitialize() {
+        smokedNamesMap.put("minecraft:dead_tube_coral", "minecraft:smoked_tube_coral");
+        smokedNamesMap.put("minecraft:dead_tube_coral_block", "minecraft:smoked_tube_coral_block");
+        smokedNamesMap.put("minecraft:dead_tube_coral_fan", "minecraft:smoked_tube_coral_fan");
+        smokedNamesMap.put("minecraft:dead_brain_coral", "minecraft:smoked_brain_coral");
+        smokedNamesMap.put("minecraft:dead_brain_coral_block", "minecraft:smoked_brain_coral_block");
+        smokedNamesMap.put("minecraft:dead_brain_coral_fan", "minecraft:smoked_brain_coral_fan");
+        smokedNamesMap.put("minecraft:dead_bubble_coral", "minecraft:smoked_bubble_coral");
+        smokedNamesMap.put("minecraft:dead_bubble_coral_block", "minecraft:smoked_bubble_coral_block");
+        smokedNamesMap.put("minecraft:dead_bubble_coral_fan", "minecraft:smoked_bubble_coral_fan");
+        smokedNamesMap.put("minecraft:dead_fire_coral", "minecraft:smoked_fire_coral");
+        smokedNamesMap.put("minecraft:dead_fire_coral_block", "minecraft:smoked_fire_coral_block");
+        smokedNamesMap.put("minecraft:dead_fire_coral_fan", "minecraft:smoked_fire_coral_fan");
+        smokedNamesMap.put("minecraft:dead_horn_coral", "minecraft:smoked_horn_coral");
+        smokedNamesMap.put("minecraft:dead_horn_coral_block", "minecraft:smoked_horn_coral_block");
+        smokedNamesMap.put("minecraft:dead_horn_coral_fan", "minecraft:smoked_horn_coral_fan");
+    }
+
+    public static TranslatableText getNewNameIfDeadCoral(ItemStack itemStack) {
+        String id = Registry.ITEM.getId(itemStack.getItem()).toString();
+        if (smokedNamesMap.containsKey(id)) {
+            TranslatableText newName = new TranslatableText(smokedNamesMap.get(id));
+            newName.setStyle(Style.EMPTY.withItalic(false));
+            return newName;
+        }
+        return null;
     }
 }
---
+++ src/main/java/io/github/sjouwer/immortalcoral/mixin/MixinAbstractCookingRecipe.java
@@ -0,0 +1,33 @@
+package io.github.sjouwer.immortalcoral.mixin;
+
+import io.github.sjouwer.immortalcoral.ImmortalCoral;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.AbstractCookingRecipe;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractCookingRecipe.class)
+public class MixinAbstractCookingRecipe {
+    @Shadow @Mutable @Final
+    ItemStack output;
+
+    @Inject(method = "<init>(Lnet/minecraft/recipe/RecipeType;Lnet/minecraft/util/Identifier;Ljava/lang/String;Lnet/minecraft/recipe/Ingredient;Lnet/minecraft/item/ItemStack;FI)V", at = @At("TAIL"))
+    public void init(RecipeType<?> type, Identifier id, String group, Ingredient input, ItemStack output, float experience, int cookTime, CallbackInfo info) {
+        if (type == RecipeType.SMOKING) {
+            TranslatableText outputName = ImmortalCoral.getNewNameIfDeadCoral(output);
+            if (outputName != null) {
+                output.setCustomName(outputName);
+                this.output = output;
+            }
+        }
+    }
+}
