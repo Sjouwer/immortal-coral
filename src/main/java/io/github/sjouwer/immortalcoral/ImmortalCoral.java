8204d9cae325e5d9b085f67aac6893950c125860
Sjouwer <sjouwer1@gmail.com>
2022-08-01 14:18:41 +0200

    Fix server crash

--- src/main/java/io/github/sjouwer/immortalcoral/ImmortalCoral.java
+++ src/main/java/io/github/sjouwer/immortalcoral/ImmortalCoral.java
@@ -1,16 +1,31 @@
 package io.github.sjouwer.immortalcoral;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.World;
 
 import static io.github.sjouwer.immortalcoral.mixin.GameRulesAccessor.invokeRegister;
 import static io.github.sjouwer.immortalcoral.mixin.BooleanRuleAccessor.invokeCreate;
 
 public class ImmortalCoral implements ModInitializer {
-    public static GameRules.Key<GameRules.BooleanRule> isCoralImmortal;
+    public static final GameRules.Key<GameRules.BooleanRule> ImmortalCoralRule = invokeRegister("immortalCoral", GameRules.Category.MISC, invokeCreate(false));
 
     @Override
     public void onInitialize() {
-        isCoralImmortal = invokeRegister("immortalCoral", GameRules.Category.MISC, invokeCreate(false));
+    }
+
+    public static boolean isCoralImmortal(BlockView world) {
+        if (world instanceof World) {
+            return ((World)world).getGameRules().getBoolean(ImmortalCoral.ImmortalCoralRule);
+        }
+        else if (world instanceof StructureWorldAccess) {
+            ServerWorld server = ((StructureWorldAccess)world).toServerWorld();
+            return server != null && server.getGameRules().getBoolean(ImmortalCoral.ImmortalCoralRule);
+        }
+
+        return false;
     }
 }
--- src/main/java/io/github/sjouwer/immortalcoral/mixin/MixinCoralBlockBlock.java
+++ src/main/java/io/github/sjouwer/immortalcoral/mixin/MixinCoralBlockBlock.java
@@ -4,7 +4,6 @@ import io.github.sjouwer.immortalcoral.ImmortalCoral;
 import net.minecraft.block.CoralBlockBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinCoralBlockBlock {
     @Inject(method = "isInWater", at = @At("HEAD"), cancellable = true)
     private void isInWater(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
-        if (((World)world).getGameRules().getBoolean(ImmortalCoral.isCoralImmortal)) {
+        if (ImmortalCoral.isCoralImmortal(world)) {
             info.setReturnValue(true);
         }
     }
